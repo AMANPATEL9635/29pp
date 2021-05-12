7d9b4ff4da6985a53003c1568c41b5f2d5384975
AMANPATEL9635 <81298439+AMANPATEL9635@users.noreply.github.com>
2021-05-12 08:11:26 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,33 @@
+class Block{
+constructor(x, y,width,height){
+    var option ={
+
+        restitutions:0.4
+        ,friction:1.0
+    }
+
+this.body=Bodies.rectangle(x,y,width,height)
+this.width=width
+this.height=height
+
+world.add(world,this.body)
+
+}
+
+display(){
+
+ var angle = this.body.angle
+var pos = this.body.position
+push()
+translate(pos.x,pox,y)
+rotate(angle)
+rectMode(CENTER)
+rect(0,0,this.width,this.height)
+pop()
+}
+
+
+
+
+
+}
